1bedd671bbff278712813adb570301dfe356541c
Taishi Otake <totake@student.42tokyo.jp>
2025-12-18 23:08:16 +0900

    fix: unset abc=123

--- builtin/unset.c
+++ builtin/unset.c
@@ -6,7 +6,7 @@
 /*   By: totake <totake@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 09:56:40 by ebichan           #+#    #+#             */
-/*   Updated: 2025/12/18 18:11:57 by totake           ###   ########.fr       */
+/*   Updated: 2025/12/18 23:00:24 by totake           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,6 +57,25 @@ int	find_env(char *key, t_data *data)
 	return (-1);
 }
 
+static int	find_env_unset(char *key, t_data *data)
+{
+	int		i;
+	size_t	len;
+
+	if (key == NULL || data->envp == NULL || ft_strchr(key, '=') != NULL)
+		return (-1);
+	len = ft_strlen(key);
+	i = 0;
+	while (data->envp[i])
+	{
+		if (ft_strncmp(data->envp[i], key, len) == 0
+			&& (data->envp[i][len] == '=' || data->envp[i][len] == '\0'))
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
 int	builtin_unset(t_cmd *cmd, t_data *data)
 {
 	int	i;
@@ -67,7 +86,7 @@ int	builtin_unset(t_cmd *cmd, t_data *data)
 		return (2);
 	while (cmd->argv[i] != NULL)
 	{
-		env_i = find_env(cmd->argv[i], data);
+		env_i = find_env_unset(cmd->argv[i], data);
 		if (env_i != -1)
 			remove_env(env_i, data);
 		i++;
